18ffe97a2ca8c6a3051e3519895065469d2dbe4e
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-10-18 15:23:53 +0300

    lab16

--- src/main/java/com/example/codefellowship/Controllers/ApplicationUserController.java
+++ src/main/java/com/example/codefellowship/Controllers/ApplicationUserController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -32,12 +33,15 @@ public class ApplicationUserController {
     }
 
     @PostMapping("/signup")
-    public RedirectView signUp(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password, @RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName, @RequestParam(value = "dateOfBirth") String dateOfBirth, @RequestParam(value = "bio") String bio) {
-        ApplicationUser newUser = new ApplicationUser(username, bCryptPasswordEncoder.encode(password), firstName, lastName, dateOfBirth, bio);
+    public RedirectView signUp(@ModelAttribute ApplicationUser applicationUser) {
+        ApplicationUser newUser = new ApplicationUser(applicationUser.getUsername(),
+                bCryptPasswordEncoder.encode(applicationUser.getPassword()),
+                applicationUser.getFirstName(), applicationUser.getLastName(),
+                applicationUser.getDateOfBirth(), applicationUser.getBio());
         applicationUserRepository.save(newUser);
 
-//        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         return new RedirectView("/login");
     }
 }
